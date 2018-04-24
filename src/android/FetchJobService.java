045d06587ffec60b809c0b430c88c01ec0abef25
Raphael Merx <raphael.merx@gmail.com>
2018-04-24 16:33:59 +0900

    Do not run job if not connectivity

--- src/android/FetchJobService.java
+++ src/android/FetchJobService.java
@@ -4,12 +4,24 @@ import android.annotation.TargetApi;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
 import android.util.Log;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 
 
 @TargetApi(21)
 public class FetchJobService extends JobService {
     @Override
     public boolean onStartJob(final JobParameters params) {
+        Context context = getApplicationContext();
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        if (activeNetworkInfo == null || !activeNetworkInfo.isConnected()) {
+            Log.d(SchedulerPlugin.TAG, "no network available, noop job");
+            return false;
+        }
+
         CompletionHandler completionHandler = new CompletionHandler() {
             @Override
             public void finish() {
@@ -17,7 +29,7 @@ public class FetchJobService extends JobService {
                 jobFinished(params, false);
             }
         };
-        SchedulerPlugin.getInstance(getApplicationContext()).onFetch(completionHandler);
+        SchedulerPlugin.getInstance(context).onFetch(completionHandler);
 
         return true;
     }
